ec63d8abbb6d272fba486ccad1911dbafd79620a
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-25 13:10:16 -0300

    fix: ajustar regras de efeitos nativos no lint

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -3,5 +3,11 @@ const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
   ...expoConfig,
-  { ignores: ['dist/**', '.expo/**', 'android/**', 'ios/**'] },
+  {
+    ignores: ['dist/**', '.expo/**', 'android/**', 'ios/**'],
+    rules: {
+      'react-hooks/purity': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
 ]);
